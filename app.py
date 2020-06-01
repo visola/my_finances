ad50739863f8cc4902467f1248143d8fa546e089
Helena <higuima13@gmail.com>
2020-06-01 18:32:40 -0300

    controle de acesso

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ def list_transactions():
     all_transactions = []
     for row in cursor:
         all_transactions.append({"id": row[0], "description": row[1]})
-    print(all_transactions)
     cnx.close() 
     return render_template("transactions/index.html", transactions=all_transactions)
 
@@ -46,16 +45,17 @@ def new_transaction():
 @app.route('/transactions/<id>')
 @login_required
 def edit_transaction(id):
-    print(id)
     cnx = mysql.connector.connect(user=MYSQL_USER, password=MYSQL_PASSWORD,
                                 host=MYSQL_HOST,
                                 database=MYSQL_DATABASE)
     cursor = cnx.cursor()
-    select_transaction = ("select * from transactions where id=%s")
-    transaction_data = (int(id),)
+    select_transaction = ("select * from transactions where id=%s and user_id=%s")
+    transaction_data = (int(id),session["id"])
     cursor.execute(select_transaction, transaction_data)
     row = cursor.fetchone()
     cnx.close()
+    if row is None :
+        return "Page not found."
     return render_template("transactions/edit.html", id=row[0], description=row[1])
     
 @app.route('/transactions/save',methods=["POST"])
@@ -66,8 +66,8 @@ def save_transactions():
                                 database=MYSQL_DATABASE)
     cursor = cnx.cursor()
     if request.form["id"] != "": 
-        update_transaction = ("update transactions set description=%s where id=%s")
-        transaction_data = (request.form["description"],request.form["id"])
+        update_transaction = ("update transactions set description=%s where id=%s and user_id = %s")
+        transaction_data = (request.form["description"],request.form["id"],session["id"])
         cursor.execute(update_transaction, transaction_data)
     else:
         insert_transaction = ("insert into transactions(description,user_id) values(%s,%s)")
@@ -75,7 +75,8 @@ def save_transactions():
         cursor.execute(insert_transaction, transaction_data)
     cnx.commit()
     cnx.close()
-    print(request.form["description"])
+    if cursor.rowcount == 0:
+        return "Sorry, there was an error."
     return redirect(url_for("list_transactions"))
 
 @app.route('/users/new')
