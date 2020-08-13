5b098e2269e7ae9d34a5063384460044b8c3bf8a
Vinicius Isola <viniciusisola@gmail.com>
2020-08-12 21:46:45 -0400

    Fix more lint errors after rebasing

--- app/__init__.py
+++ app/__init__.py
@@ -36,13 +36,27 @@ def list_transactions():
                                   host=MYSQL_HOST,
                                   database=MYSQL_DATABASE)
     cursor = cnx.cursor()
-    select_transactions = ("select t.id,t.description,c.category,t.date,t.value,a.name from transactions t left join categories c on t.category_id = c.id join accounts a on t.source_accnt_id = a.id where t.user_id=%s order by date desc")
+    select_transactions = ('''
+        select t.id,t.description,c.category,t.date,t.value,a.name
+        from transactions t
+        left join categories c on t.category_id = c.id
+        join accounts a on t.source_accnt_id = a.id
+        where t.user_id=%s
+        order by date desc
+    ''')
     session_id_data = (session["id"],)
     cursor.execute(select_transactions, session_id_data)
     all_transactions = []
     for row in cursor:
-        all_transactions.append({"id": row[0], "description": row[1], "category": row[2], "date": row[3], "value": row[4] , "source_account":row[5]})
-    cnx.close() 
+        all_transactions.append({
+            "id": row[0],
+            "description": row[1],
+            "category": row[2],
+            "date": row[3],
+            "value": row[4],
+            "source_account":row[5]
+        })
+    cnx.close()
     return render_template("transactions/index.html", transactions=all_transactions)
 
 @app.route('/transactions/new')
@@ -73,9 +87,9 @@ def new_transaction():
 @login_required
 def edit_transaction(transaction_id):
     cnx = mysql.connector.connect(user=MYSQL_USER, password=MYSQL_PASSWORD,
-                                host=MYSQL_HOST,
-                                database=MYSQL_DATABASE)
-    cursor = cnx.cursor()    
+                                  host=MYSQL_HOST,
+                                  database=MYSQL_DATABASE)
+    cursor = cnx.cursor()
     select_categories = ("select id,category from categories where user_id=%s")
     category_data = (session["id"],)
     cursor.execute(select_categories, category_data)
@@ -84,15 +98,19 @@ def edit_transaction(transaction_id):
     account_data = (session["id"],)
     cursor.execute(select_accounts, account_data)
     all_accounts = cursor.fetchall()
-    select_transaction = ("select id,description,category_id,date,value,source_accnt_id,link_id from transactions where id=%s and user_id=%s")    
-    transaction_data = (int(id),session["id"])
-    cursor.execute(select_transaction, transaction_data)    
+    select_transaction = ('''
+        select id,description,category_id,date,value,source_accnt_id,link_id
+        from transactions
+        where id=%s and user_id=%s
+    ''')
+    transaction_data = (int(transaction_id), session["id"])
+    cursor.execute(select_transaction, transaction_data)
     row = cursor.fetchone()
     linked_transaction_accnt = None
     if row[6] is not None:
         linked_account = ("select source_accnt_id from transactions where link_id =%s and id<> %s")
-        link_id_data = (row[6],int(id))
-        cursor.execute(linked_account,link_id_data)
+        link_id_data = (row[6], int(transaction_id))
+        cursor.execute(linked_account, link_id_data)
         linked_transaction_accnt = cursor.fetchone()[0]
     cnx.close()
     if row is None:
@@ -100,39 +118,39 @@ def edit_transaction(transaction_id):
     return render_template(
         "transactions/edit.html",
         accounts=all_accounts,
-        categories=all_categories, 
-        category_id = row[2], 
-        date=row[3], 
-        description=row[1], 
-        destination_accnt_id = linked_transaction_accnt,
-        id=row[0], 
-        source_accnt_id = row[5], 
-        value=row[4], 
-        link_id= row[6]
+        categories=all_categories,
+        category_id=row[2],
+        date=row[3],
+        description=row[1],
+        destination_accnt_id=linked_transaction_accnt,
+        id=row[0],
+        source_accnt_id=row[5],
+        value=row[4],
+        link_id=row[6]
     )
 
 @app.route('/transactions/save', methods=["POST"])
 @login_required
 def save_transactions():
     cnx = mysql.connector.connect(user=MYSQL_USER, password=MYSQL_PASSWORD,
-                                host=MYSQL_HOST,
-                                database=MYSQL_DATABASE)
+                                  host=MYSQL_HOST,
+                                  database=MYSQL_DATABASE)
     cursor = cnx.cursor()
 
     passed_id = request.form["id"]
-    
+
     value = request.form["value"]
     if value == "":
         return "Value can not be empty."
     if session['preference'] == "pt-br":
         value = value.replace(",", ".")
     else:
-        value = value.replace(",","")
+        value = value.replace(",", "")
     reverse_value = float(value)*-1.0
-    
-    date = request.form["date"] 
-    if session['preference']== "pt-br":
-        date = datetime.strptime(request.form["date"],'%d/%m/%Y')
+
+    transaction_date = request.form["date"]
+    if session['preference'] == "pt-br":
+        transaction_date = datetime.strptime(request.form["date"], '%d/%m/%Y')
     elif session['preference'] == "en-us":
         transaction_date = datetime.strptime(request.form["date"], '%m/%d/%Y')
     else:
@@ -140,23 +158,23 @@ def save_transactions():
     category_id = request.form['category_id']
     if category_id == "-1":
         category_id = None
-    
+
     validate_source_accnt = ("select id from accounts where id = %s and user_id = %s")
-    source_accnt_data = (request.form["source_accnt_id"],session["id"])
-    cursor.execute(validate_source_accnt,source_accnt_data)
+    source_accnt_data = (request.form["source_accnt_id"], session["id"])
+    cursor.execute(validate_source_accnt, source_accnt_data)
     cursor.fetchone()
     if cursor.rowcount == 0:
         cnx.close()
         return "Sorry, there was an error. 1"
     if request.form["destination_accnt_id"] != "-1":
         validate_destination_accnt = ("select id from accounts where id = %s and user_id = %s")
-        destination_accnt_data = (request.form["destination_accnt_id"],session["id"])
-        cursor.execute(validate_destination_accnt,destination_accnt_data)
+        destination_accnt_data = (request.form["destination_accnt_id"], session["id"])
+        cursor.execute(validate_destination_accnt, destination_accnt_data)
         cursor.fetchone()
         if cursor.rowcount == 0:
             cnx.close()
             return "Sorry, there was an error. 2"
-    
+
     select_categories = ("select category from categories where user_id=%s and id=%s")
     categories_data = (session["id"], category_id)
     cursor.execute(select_categories, categories_data)
@@ -164,10 +182,20 @@ def save_transactions():
     if cursor.rowcount == 0 and category_id is not None:
         cnx.close()
         return "Sorry, there was an error. 3"
-    
+
     reverse_transaction_id = None
-    if passed_id != "": 
-        select_transaction = ("SELECT t.id, t.link_id, r.id FROM transactions t LEFT OUTER JOIN transactions r ON r.link_id = t.link_id and t.link_id IS NOT NULL AND t.user_id = r.user_id and t.id<>r.id WHERE t.id = %s AND t.user_id = %s")
+    link_id = None
+    if passed_id != "":
+        select_transaction = ('''
+            SELECT t.id, t.link_id, r.id
+            FROM transactions t
+            LEFT OUTER JOIN transactions r
+                ON r.link_id = t.link_id
+                and t.link_id IS NOT NULL
+                AND t.user_id = r.user_id
+                and t.id<>r.id
+            WHERE t.id = %s AND t.user_id = %s
+        ''')
         transaction_data = (passed_id, session["id"])
         cursor.execute(select_transaction, transaction_data)
         row = cursor.fetchone()
@@ -176,19 +204,43 @@ def save_transactions():
             return "Sorry, there was an error. 4"
         link_id = row[1]
         reverse_transaction_id = row[2]
-        
-    if request.form["destination_accnt_id"] == "-1":
-        link_id = None
-    elif link_id is None:
+
+    if request.form["destination_accnt_id"] != "-1" and link_id is None:
         link_id = str(uuid.uuid4())
-    
-    if passed_id != "": 
-        update_transaction = ("update transactions set description=%s, category_id=%s, date=%s, value=%s, source_accnt_id=%s, link_id=%s where id=%s and user_id = %s")
-        transaction_data = (request.form["description"], category_id, date, value, request.form["source_accnt_id"], link_id, request.form["id"], session["id"])
+
+    if passed_id != "":
+        update_transaction = ('''
+            update transactions
+            set description=%s, category_id=%s, date=%s, value=%s, source_accnt_id=%s, link_id=%s
+            where id=%s
+                and user_id = %s
+        ''')
+        transaction_data = (
+            request.form["description"],
+            category_id,
+            transaction_date,
+            value,
+            request.form["source_accnt_id"],
+            link_id,
+            request.form["id"],
+            session["id"]
+        )
         cursor.execute(update_transaction, transaction_data)
-    else: 
-        insert_transaction = ("insert into transactions(description, user_id, category_id, date, value, source_accnt_id, link_id) values(%s, %s, %s, %s, %s, %s, %s)")                
-        transaction_data = (request.form["description"], session["id"], category_id, date, value, request.form["source_accnt_id"], link_id)
+    else:
+        insert_transaction = ('''
+            insert into transactions
+            (description, user_id, category_id, date, value, source_accnt_id, link_id)
+            values (%s, %s, %s, %s, %s, %s, %s)
+        ''')
+        transaction_data = (
+            request.form["description"],
+            session["id"],
+            category_id,
+            transaction_date,
+            value,
+            request.form["source_accnt_id"],
+            link_id
+        )
         cursor.execute(insert_transaction, transaction_data)
 
     if link_id is None:
@@ -198,19 +250,42 @@ def save_transactions():
             cursor.execute(delete_linked_transactions, delete_data)
     else:
         if reverse_transaction_id is not None:
-            update_transaction = ("update transactions set description=%s, category_id=%s, date=%s, value=%s, source_accnt_id=%s, link_id=%s where id=%s and user_id = %s")            
-            transaction_data = (request.form["description"], category_id, date, reverse_value, request.form["destination_accnt_id"], link_id, reverse_transaction_id, session["id"])
+            update_transaction = ('''
+                update transactions
+                set description=%s, category_id=%s, date=%s, value=%s, source_accnt_id=%s, link_id=%s
+                where id=%s and user_id = %s
+            ''')
+            transaction_data = (
+                request.form["description"],
+                category_id,
+                transaction_date,
+                reverse_value,
+                request.form["destination_accnt_id"],
+                link_id,
+                reverse_transaction_id,
+                session["id"]
+            )
             cursor.execute(update_transaction, transaction_data)
         else:
-            insert_transaction = ("insert into transactions (description,user_id,category_id,date,value,source_accnt_id,link_id) values(%s,%s,%s,%s,%s,%s,%s)")
-            transaction_data_insert = (request.form["description"], session["id"], category_id, date, reverse_value, request.form["destination_accnt_id"], link_id)
+            insert_transaction = ('''
+                insert into transactions
+                (description,user_id,category_id,date,value,source_accnt_id,link_id)
+                values(%s,%s,%s,%s,%s,%s,%s)
+            ''')
+            transaction_data_insert = (
+                request.form["description"],
+                session["id"],
+                category_id,
+                transaction_date,
+                reverse_value,
+                request.form["destination_accnt_id"],
+                link_id
+            )
             cursor.execute(insert_transaction, transaction_data_insert)
 
     cnx.commit()
     cnx.close()
     return redirect(url_for("list_transactions"))
-
-
 
 @app.route('/users/new')
 def create_user():
@@ -335,26 +410,26 @@ def save_categories():
 @login_required
 def dashboard():
     cnx = mysql.connector.connect(user=MYSQL_USER, password=MYSQL_PASSWORD,
-                                host=MYSQL_HOST,
-                                database=MYSQL_DATABASE)
+                                  host=MYSQL_HOST,
+                                  database=MYSQL_DATABASE)
     cursor = cnx.cursor()
     count_c = ('''
-            select count(t.id),c.category
-            from transactions t
-            join categories c
-            where t.date between %s and %s
+        select count(t.id),c.category
+        from transactions t
+        join categories c
+        where t.date between %s and %s
             and t.category_id=c.id
             and t.user_id =%s
-            group by t.category_id
-            order by c.category asc
-        ''')
+        group by t.category_id
+        order by c.category asc
+    ''')
     end_date_c = date.today()
-    start_date_c= date.fromordinal(end_date_c.toordinal()-30) 
-    select_count_c= (start_date_c,end_date_c,session["id"])
-    cursor.execute(count_c,select_count_c)
+    start_date_c = date.fromordinal(end_date_c.toordinal()-30)
+    select_count_c = (start_date_c, end_date_c, session["id"])
+    cursor.execute(count_c, select_count_c)
     counts = {}
     for row in cursor:
-        counts[row[1]]=row[0]
+        counts[row[1]] = row[0]
     count_c = ('''
             select count(t.id),c.category
             from transactions t
@@ -367,23 +442,34 @@ def dashboard():
             order by c.category asc
         ''')
     end_date_c = date.today()
-    start_date_c= date.fromordinal(end_date_c.toordinal()-30) 
-    select_count_c= (start_date_c,end_date_c,session["id"])
-    cursor.execute(count_c,select_count_c)
+    start_date_c = date.fromordinal(end_date_c.toordinal()-30)
+    select_count_c = (start_date_c, end_date_c, session["id"])
+    cursor.execute(count_c, select_count_c)
     for row in cursor:
-        counts[row[1]]= counts[row[1]] - int(row[0]/2)
-    count_a = ("select count(t.id),a.name from transactions t join accounts a where t.date between %s and %s and t.source_accnt_id=a.id and t.user_id =%s group by t.source_accnt_id order by name asc")
+        counts[row[1]] = counts[row[1]] - int(row[0]/2)
+    count_a = ('''
+        select count(t.id),a.name
+        from transactions t
+        join accounts a
+        where t.date between %s and %s
+            and t.source_accnt_id=a.id
+            and t.user_id = %s
+        group by t.source_accnt_id
+        order by name asc
+    ''')
     end_date_a = date.today()
-    start_date_a= date.fromordinal(end_date_c.toordinal()-30)
-    select_count_a= (start_date_a,end_date_a,session["id"])
-    cursor.execute(count_a,select_count_a)
+    start_date_a = date.fromordinal(end_date_c.toordinal()-30)
+    select_count_a = (start_date_a, end_date_a, session["id"])
+    cursor.execute(count_a, select_count_a)
     all_accounts = []
     for row in cursor:
         all_accounts.append({"count": row[0], "account": row[1]})
-    cnx.close() 
-    return render_template("home_page/index.html", count_categories=counts, count_accounts=all_accounts)
-
-#TODO: mostrar quantas transações não possuem categorias; 
+    cnx.close()
+    return render_template(
+        "home_page/index.html",
+        count_categories=counts,
+        count_accounts=all_accounts
+    )
 
 @app.route('/accounts')
 @login_required
