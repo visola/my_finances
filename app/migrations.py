061752383be1b015df621f9d34538d27290e0d75
Vinicius Isola <viniciusisola@gmail.com>
2021-01-13 23:41:46 -0500

    Do not run migrations if DB is misconfigured

--- app/migrations.py
+++ app/migrations.py
@@ -3,6 +3,10 @@ from alembic import command
 from alembic.config import Config
 
 def run_migrations(migrations_dir: str, dsn: str) -> None:
+    if "{DATABASE_HOST}" in dsn:
+        print("DB is misconfigured. Not running migrations.")
+        return
+
     print(f"Running migrations from {migrations_dir}")
     alembic_cfg = Config()
     alembic_cfg.config_file_name = f"{os.getcwd()}/alembic.ini"
